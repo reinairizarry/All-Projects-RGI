f9d3a082153a1ea2f24638d33bc103565e2599fc
Reina Irizarry <reinalizirizarry@gmail.com>
2018-10-21 15:57:17 -0500

    test

---
+++ gettingStarted.js
@@ -0,0 +1,9 @@
+function opengettingStarted() {
+    var help = document.getElementById('helpID');
+    help.style.display = "block";
+}
+
+function closegettingStarted() {
+    var help = document.getElementById('helpID');
+    help.style.display = "none";
+}
--- map-mania-lite.js
+++ map-mania-lite.js
@@ -6,16 +6,41 @@
 //og
 var gMap;
 
+      // This example creates a simple polygon representing the Bermuda Triangle.
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('myMapID'), {
+          zoom: 5,
+          center: {lat: 24.886, lng: -70.268},
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        });
+
+        // Define the LatLng coordinates for the polygon's path.
+        var triangleCoords = [
+          {lat: 25.774, lng: -80.190},
+          {lat: 18.466, lng: -66.118},
+          {lat: 32.321, lng: -64.757},
+          {lat: 25.774, lng: -80.190}
+        ];
+
+        // Construct the polygon.
+        var bermudaTriangle = new google.maps.Polygon({
+          paths: triangleCoords,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35
+        });
+        bermudaTriangle.setMap(map);
+      }
+
 var atlasObscura = [
-    {content:'Crooked Forest,Poland>', coordinates:{lat:53.213889,lng:14.475}, iconImagePath:"tree.png"},
-    {content:'Witch House, Salem', coordinates:{lat:42.5215,lng:-70.8989}, iconImagePath:"house.png"},
-    {content:'Bermuda Triangle,North Alanctic Ocean', coordinates:{lat:27.132481 ,lng:73.086548}, iconImagePath:"triangle.png"},//add polygon
-    {content:'Pyramids of Giza, Egypt', coordinates:{lat:29.9792,lng:31.1342}, iconImagePath:"flag.png"},
-    {content:'Prague, Czechia', coordinates:{lat:50.0755,lng:14.4378}, iconImagePath:"flag.png"},
-    {content:'Nelson, New Zealand', coordinates:{lat:-41.2706,lng:173.2840}, iconImagePath:"flag.png"},
-    {content:'Krakow, Poland', coordinates:{lat:50.0647,lng:19.9450}, iconImagePath:"flag.png"},
-    {content:'Zakopane, Poland', coordinates:{lat:49.2992,lng:19.9496}, iconImagePath:"flag.png"},
-    {content:'Canoe Bay, WI', coordinates:{lat:45.3306,lng:-91.4918}, iconImagePath:"flag.png"}
+    {content:"Crooked Forest,Poland", coordinates:{lat:53.213889,lng:14.475}, iconImagePath:"tree.png"},
+    {content:"Witch House, Salem", coordinates:{lat:42.5215,lng:-70.8989}, iconImagePath:"house.png"},
+    {content:"Bermuda Triangle,North Alanctic Ocean", coordinates:{lat:27.132481 ,lng:73.086548}, iconImagePath:"triangle.png"},//add polygon
+    {content:"Pyramids of Giza, Egypt", coordinates:{lat:29.9792,lng:31.1342}, iconImagePath:"flag.png"},
+
 ];
 var currentPlaceIndex = atlasObscura.length-1;
 var currentPlace = atlasObscura[currentPlaceIndex];
@@ -40,6 +65,11 @@ function initMap() {
     var marker2 = new google.maps.Marker({position:{lat:  53.214311,lng:14.475698}, map:gMap});
     marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/tree.png');
 
+    var marker2 = new google.maps.Marker({position:{lat:  29.9792,lng:31.1342}, map:gMap});
+    marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/campground.png');
+
+    var marker2 = new google.maps.Marker({position:{lat:  29.9792,lng:31.1342}, map:gMap});
+    marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/campground.png');
 
     var infoWindow = new google.maps.InfoWindow({content:'Crooked Forest, Poland'});
     marker2.addListener('click', function() {
@@ -49,7 +79,11 @@ function initMap() {
 
     SetHint("Hint 1");
     SetScore(score);
+
+    SetHint("Hint 2");
+    SetScore(score);
 }
+
 
 function updateGame() {
     console.log('function UpdateGame() google-maps-step-03!');
@@ -66,6 +100,7 @@ function updateGame() {
 
 function SetHint(hint) {
     document.getElementById("hint-id").value = hint;
+    console.log('You are getting warmer!');
 }
 
 function SetScore() {
