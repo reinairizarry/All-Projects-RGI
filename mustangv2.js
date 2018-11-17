4b34b925f9bc3bc4956d33bc634003ea9c487d5b
Reina Irizarry <reinalizirizarry@gmail.com>
2018-11-17 11:10:55 -0600

    test

---
+++ mustangv2.js
@@ -0,0 +1,279 @@
+
+var contactURLArray = [];
+var contactArray = [];
+var loadingContact = 0;
+var currentContactIndex = 0;
+var lName = ["Irizarry","Porter","Goff","Murphy","Hyland","Towner","Lesiak &amp; Colvin","Schober","Rivera"];
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+function initApplication() {
+    console.log('Mustang v2 is starting!');
+    document.getElementById("nameID").value = "";
+    document.getElementById("emailID").value = "";
+    document.getElementById("cityID").value = "";
+    document.getElementById("stateID").value = "";
+    document.getElementById("zipID").value = "";
+}
+
+function setStatus(status) {
+    document.getElementById("statusID").innerHTML = status;
+}
+
+function zipBlurFunction() {
+    ZipToCityState();
+}
+
+function importContacts() {
+    console.log("importContacts()");
+    loadIndexAndContacts();
+}
+
+function saveContactsToServer() {
+    console.log("saveContactsToServer()");
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('Response: ' + this.responseText);
+            setStatus(this.responseText)
+        }
+    };
+    xmlhttp.open("POST", "save-contacts.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("contacts=" + JSON.stringify(contactArray));
+}
+
+function loadContactsFromServer() {
+    console.log("loadContactsFromServer()");
+
+    // Clear the current contacts.
+    contactArray.length = 0;
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            contactArray = JSON.parse(this.responseText);
+            setStatus("Loaded contacts (" + contactArray.length + ")");
+
+            currentContactIndex = 0;
+            viewCurrentContact()
+        }
+    };
+
+    xmlhttp.open("GET", "load-contacts.php", true);
+    xmlhttp.send();
+}
+
+function logContacts() {
+    console.log("ContactArray: ");
+    console.log(contactArray);
+}
+
+function viewCurrentContact() {
+    currentContact = contactArray[currentContactIndex];
+    console.log(currentContact);
+    document.getElementById("nameID").value = currentContact.preferredName;
+    document.getElementById("emailID").value = currentContact.email;
+    document.getElementById("cityID").value = currentContact.city;
+    document.getElementById("stateID").value = currentContact.state;
+    document.getElementById("zipID").value = currentContact.zip;
+
+    // Todo: Add additional fields.
+    document.getElementById("statusID").innerHTML = "Viewing contact " + (currentContactIndex+1) + " of " + contactArray.length;
+}
+
+function previous() {
+    if (currentContactIndex > 0) {
+        currentContactIndex--;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+
+    // Todo: Disable previous button when currentContactIndex equal to 0.
+}
+
+function next() {
+    if (currentContactIndex < (contactArray.length-1)) {
+        currentContactIndex++;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+    
+
+}
+
+function add() {
+    console.log('add()**');
+
+    // Todo: Implement add functionality by inserting new element into array.
+}
+
+function remove() {
+    console.log('remove()');
+    var lName = ["Irizarry","Porter","Goff","Murphy","Hyland","Towner","Lesiak &amp; Colvin","Schober","Rivera"];
+months.splice(1, 0, 'Owner');
+// inserts at 1st index position
+console.log(lname);
+
+months.splice(4, 1, 'Goff');
+// replaces 1 element at 4th index
+console.log(months);
+}
+
+function ZipToCityState() {
+    var zip = document.getElementById("zipID").value
+    console.log("zip:"+zip);
+
+    console.log("function getPlace(zip) { ... }");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            console.log("ZipToCityState Result:" + result);
+            var place = result.split(', ');
+            if (document.getElementById("cityID").value == "")
+                document.getElementById("cityID").value = place[0];
+            if (document.getElementById("stateID").value == "")
+                document.getElementById("stateID").value = place[1];
+        }
+    }
+    xhr.open("GET", "zip-to-city-state.php?zip=" + zip);
+    xhr.send(null);
+}
+
+function loadIndexAndContacts() {
+    // Load previous Mustang index file.
+    var indexRequest = new XMLHttpRequest();
+    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
+    indexRequest.onload = function() {
+        console.log("Index JSON:" + indexRequest.responseText);
+        document.getElementById("indexID").innerHTML = indexRequest.responseText;
+        contactIndex = JSON.parse(indexRequest.responseText);
+        for (i=0; i<contactIndex.length; i++) {
+            contactURLArray.push(contactIndex[i].ContactURL);
+        }
+        console.log("ContactURLArray: " + JSON.stringify(contactURLArray));
+        loadContacts();
+    }
+    indexRequest.send();
+}
+
+function loadContacts() {
+
+    contactArray.length = 0;
+    loadingContact = 0;
+    if (contactURLArray.length > loadingContact) {
+        loadNextContact(contactURLArray[loadingContact]);
+    }
+}
+
+function loadNextContact(URL) {
+    console.log("URL: " + URL);
+    contactRequest = new XMLHttpRequest();
+    contactRequest.open('GET', URL);
+    contactRequest.onload = function() {
+        var contact = JSON.parse(contactRequest.responseText);
+        contactArray.push(contact);
+
+        document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
+        document.getElementById("statusID").innerHTML = "Loading " + contact.firstName + " " + contact.lastName;
+
+        loadingContact++;
+        if (contactURLArray.length > loadingContact) {
+            loadNextContact(contactURLArray[loadingContact]);
+        }
+        else {
+            document.getElementById("statusID").innerHTML = "Contacts Loaded (" + contactURLArray.length + ")";
+            viewCurrentContact()
+        }
+    }
+    contactRequest.send();
+}
